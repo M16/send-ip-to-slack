8f2879665bfef522b5f54efaf1037413a186fe22
majed <majed@prine.ch>
2020-08-31 18:51:51 +0200

    fixed loading config when excuted by cronjob

--- ip-finder.py
+++ ip-finder.py
@@ -4,15 +4,18 @@ import time
 import logging
 import configparser
 import requests
+import pathlib
 
 logging.basicConfig(level=logging.INFO)
 config = configparser.ConfigParser()
 
 ip_refelctor_response = requests.get('http://ip.fyr.io/', timeout=10)
 
-config.read('config.ini')
+config_path = str(pathlib.Path(__file__).parent.absolute()) + '/config.ini'
 
-message = str(config['device']['name']) + " is connected with IP: " + ip_refelctor_response.content
+config.read(config_path)
+
+message = str(config['device']['name']) + " is connected with IP:" + str(ip_refelctor_response.content)
 
 slackQueryData = '''{
     "channel": "#''' + config['slack']['channel'] + '''",
