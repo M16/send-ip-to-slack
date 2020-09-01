f326ac6412419f01e80e57e8c440afe28e06d1e4
majed <majed@prine.ch>
2020-09-01 15:28:07 +0200

    added report of ifconfig

--- ip-finder.py
+++ ip-finder.py
@@ -5,6 +5,7 @@ import logging
 import configparser
 import requests
 import pathlib
+import subprocess
 
 logging.basicConfig(level=logging.INFO)
 config = configparser.ConfigParser()
@@ -15,14 +16,16 @@ config_path = str(pathlib.Path(__file__).parent.absolute()) + '/config.ini'
 
 config.read(config_path)
 
-message = str(config['device']['name']) + " is connected with IP:" + str(ip_refelctor_response.content)
+output = subprocess.run(['ifconfig','-a'],check=True, stdout=subprocess.PIPE, universal_newlines=True)
+
+message = str(config['device']['name']) + " is connected with IP:" + str(ip_refelctor_response.content) + '.\n\n $ ifconfig -a results: \n\n' + str(output)
 
 slackQueryData = '''{
     "channel": "#''' + config['slack']['channel'] + '''",
     "username": "''' + config['slack']['username'] + '''",
     "icon_emoji": ":''' + config['slack']['icon_emoji'] + ''':",
     "type": "''' + config['slack']['type'] + '''",
-    "text": "''' + message + '''",
+    "text": "''' + str(message) + '''",
     }'''
 
 response = requests.post(config['slack']['url'], data=slackQueryData)
